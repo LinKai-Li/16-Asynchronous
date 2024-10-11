f160d468033f52de9d69b3a7c1d08b33878fce39
Link <Link@Links-MacBook-Pro-14>
2024-10-11 19:15:51 +1100

    256. Throwing Errors Manually

--- script.js
+++ script.js
@@ -32,22 +32,32 @@ const renderError = function (msg) {
 
 ///////////////////////////////////////
 
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
   // Country 1
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then((response) => response.json())
+  getJSON(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    "Country not found"
+  )
     .then((data) => {
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
 
-      if (!neighbour) return;
+      if (!neighbour) throw new Error("No neighbour found!");
 
       // Country 2
-      return fetch(
-        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+      return getJSON(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+        "Country not found"
       );
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data, "neighbour"))
     .catch((err) => {
       console.error(`${err} 💥💥💥`);
@@ -60,4 +70,4 @@ btn.addEventListener("click", function () {
   getCountryData("portugal");
 });
 
-getCountryData("dsfddsfsdf");
+getCountryData("australia");
