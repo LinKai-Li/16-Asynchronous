4c520199af60831978fee6678b202532733f2c06
Link <Link@Links-MacBook-Pro-14>
2024-10-11 18:17:07 +1100

    253. Consuming Promises

--- script.js
+++ script.js
@@ -5,90 +5,32 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
-// const renderCountry = function (data, className = "") {
-//   const html = `<article class="country ${className}">
-//             <img class="country__img" src="${data.flag}" />
-//             <div class="country__data">
-//               <h3 class="country__name">${data.name}</h3>
-//               <h4 class="country__region">${data.region}</h4>
-//               <p class="country__row"><span>👫</span>${(
-//                 +data.population / 1000000
-//               ).toFixed(1)} people</p>
-//               <p class="country__row"><span>🗣️</span>${
-//                 data.languages[0].name
-//               }</p>
-//               <p class="country__row"><span>💰</span>${
-//                 data.currencies[0].name
-//               }</p>
-//             </div>
-//           </article>`;
+const renderCountry = function (data, className = "") {
+  const html = `<article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+            </div>
+          </article>`;
 
-//   countriesContainer.insertAdjacentHTML("beforeend", html);
-//   countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
 
-// const getCountryAndNeighbour = function (country) {
-//   // AJAX call country 1
-//   const request = new XMLHttpRequest();
-//   request.open(
-//     "GET",
-//     `https://countries-api-836d.onrender.com/countries/name/${country}`
-//   );
-//   request.send();
+const getCountryData = function (country) {
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
 
-//   request.addEventListener("load", function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     // Render country 1
-//     renderCountry(data);
-
-//     // Get neighbour country (2)
-//     const [neighbour] = data.borders;
-
-//     if (!neighbour) return;
-
-//     // AJAX call country 2
-//     const request2 = new XMLHttpRequest();
-//     request2.open(
-//       "GET",
-//       `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-//     );
-//     request2.send();
-
-//     request2.addEventListener("load", function () {
-//       const data2 = JSON.parse(this.responseText);
-//       console.log(data2);
-
-//       renderCountry(data2, "neighbour");
-//     });
-//   });
-// };
-
-// // getCountryAndNeighbour("portugal");
-// getCountryAndNeighbour("usa");
-
-// setTimeout(() => {
-//   console.log("1 second passed");
-//   setTimeout(() => {
-//     console.log("2 second passed");
-//     setTimeout(() => {
-//       console.log("3 second passed");
-//       setTimeout(() => {
-//         console.log("4 second passed");
-//       }, 1000);
-//     }, 1000);
-//   }, 1000);
-// }, 1000);
-
-// const request = new XMLHttpRequest();
-// request.open(
-//   "GET",
-//   `https://countries-api-836d.onrender.com/countries/name/${country}`
-// );
-// request.send();
-
-const request = fetch(
-  "https://countries-api-836d.onrender.com/countries/name/portugal"
-);
-console.log(request);
+getCountryData("portugal");
